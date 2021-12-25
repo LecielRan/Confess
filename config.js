d56457a09ea0956f47657bfadacbfacad7581970
LecielRan <96631098+LecielRan@users.noreply.github.com>
2021-12-25 14:27:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
 const CONFIG = {
     titleWeb: "Leciel",
-    introTitle: 'Gửi tới con nhóc đặc biệt được sinh ra trong ngày đặc biệt!'
+    introTitle: 'Leciel',
+    introDesc: `Gửi tới con nhóc đặc biệt được sinh ra trong ngày đặc biệt!`,
+    btnIntro: 'Nhấn vô đây!',
     title: 'Happy Birthday',
     desc: 'Chúc em luôn tự tin, thật nhiều sức khỏe, thật nhiều may mắn sẽ đến với em . Cho dù có thêm nhiều khó khăn, hay chuyện buồn nào nữa, hãy luôn nhớ - em có tôi, Leciel luôn đứng về phía em , ủng hộ em ! ',
     btnYes: 'Cảm ơn',
     btnNo: 'Không cảm ơn',
     question: 'Rõ rồi chứ?',
-    btnReply: 'trả lời',
+    btnReply: 'Trả lời',
     reply: 'Em nên nói sao?!',
     mess: 'Phải nói là " tuân lệnh" . Leciel là nhất, Leciel là số 1!',
-    messDesc: 'Leciel luôn ở đây khi em cần',
-    btnAccept: 'Tuân Lệnh',
-    messLink: 'https://www.facebook.com/HaitaniRan.Leciel'
+    messDesc: 'Leciel luôn ở đây khi em cần!',
+    btnAccept: 'Tuân lệnh',
+    messLink: 'https://www.facebook.com/HaitaniRan.Leciel' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
